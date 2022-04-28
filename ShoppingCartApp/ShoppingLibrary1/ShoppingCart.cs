e0334a2665e75a076eeec96fe447902e5b66185c
Rahdee <4283301@myuwc.ac.za>
2022-04-28 15:49:56 +0200

    Starting SubTotalVat

---
+++ ShoppingCartApp/ShoppingLibrary1/ShoppingCart.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingLibrary1
+{
+    // follow the steps as object orientation
+    public class ShoppingCart
+    {
+        List<ShoppingCartItem> _ShoppingCartItems = new List<ShoppingCartItem>();
+
+        public ShoppingCart()
+        {
+            
+        }
+
+        public void AddShoppingCartItems(ShoppingCartItem item)
+        {
+            _ShoppingCartItems.Add(item);
+        }
+
+        public List<ShoppingCartItem> MyCartItems()
+        {
+           return _ShoppingCartItems;
+
+        }
+
+        public double GetSubtotal()
+        {
+            double subtotal = 0;
+
+            foreach (ShoppingCartItem Item in _ShoppingCartItems)
+            {
+                Item.product();
+                Item.Price();
+
+            }
+            return subtotal;
+        }
+        public double GetVat()
+        {
+
+        }
+    }   
+
+}
